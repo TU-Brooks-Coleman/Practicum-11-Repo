8f196c7004ef3a94604b66fb505183a21f8e57fe
TU-Brooks_Coleman <brooksmcoleman@gmail.com>
2024-09-03 11:27:28 -0400

    Done

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 // Practicum 11 (Largest of Three Program)
 // (TO COMPLETE)
+import java.util.Scanner;
 
 public class Main {
 
@@ -8,11 +9,31 @@ public class Main {
         // var declarations
         double num1, num2, num3, largest;
 
-        // prompt user for three numbers
-       
+        // initialize scanner class
+        Scanner sc = new Scanner(System.in);
 
-        // determine largest and assign to variable largest
+        // prompt user for three numbers
+        System.out.print("Please enter the first number: ");
+        num1 = sc.nextDouble();
+
+        System.out.print("Please enter the second number: ");
+        num2 = sc.nextDouble();
+
+        System.out.print("Please enter the third number: ");
+        num3 = sc.nextDouble();
         
+        // determine largest and assign to variable largest
+        if (num1 > num2) {
+            if (num1 > num3) {
+                largest = num1;
+            } else {
+                largest = num3;
+            }
+        } else if (num2 > num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
 
         // display results
         System.out.println("The largest of " + num1 + ", " + num2 +
